40642799051078fba2c44657a5141938b19be07a
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-11-30 22:00:28 +0530

    Improve blog scrapping code

--- blog_scrapping.py
+++ blog_scrapping.py
@@ -1,21 +1,42 @@
 import requests
-from csv import writer
 from bs4 import BeautifulSoup
+from csv import writer
 
-response = requests.get("https://www.rithmschool.com/blog")
-soup = BeautifulSoup(response.text, "html.parser")
-all_articles = soup.find_all("article")
+all_blogs_scrapped = []
 
-with open("blog_data.csv", "w") as csv_file:
+base_url = "https://www.rithmschool.com"
+page_url = "/blog?page=1"
+
+while page_url:
+    res = requests.get(f"{base_url}{page_url}")
+    soup = BeautifulSoup(res.text, "html.parser")
+    all_blogs = soup.find_all("article")
+
+    for blog in all_blogs:
+        anchor_tag = blog.find("a")
+        time_tag = blog.find("time")
+
+        blog_date = time_tag["datetime"]
+        blog_title = anchor_tag.get_text()
+        blog_slug = anchor_tag["href"]
+        blog_link = f"{base_url}{blog_slug}"
+
+        all_blogs_scrapped.append({
+            "blog_date": blog_date,
+            "blog_title": blog_title,
+            "blog_link": blog_link,
+        })
+
+    pagination_container = soup.find(attrs={"class": "pagination"})
+    next_button = pagination_container.find(attrs={"class": "next"})
+    page_url = next_button.find("a")["href"] if next_button else None
+
+with open("all_blogs_scrapped.csv", "w") as csv_file:
     csv_writer = writer(csv_file)
-    csv_writer.writerow(["title", "link", "date"])
+    csv_writer.writerow(["blog_date", "blog_title", "blog_link"])
 
-    for article in all_articles:
-        anchor_tag = article.find("a")
-        time_tag = article.find("time")
-
-        title = anchor_tag.get_text()
-        link = anchor_tag["href"]
-        date = time_tag["datetime"]
-
-        csv_writer.writerow([title, link, date])
+    for single_blog_scrapped in all_blogs_scrapped:
+        blog_date = single_blog_scrapped["blog_date"]
+        blog_title = single_blog_scrapped["blog_title"]
+        blog_link = single_blog_scrapped["blog_link"]
+        csv_writer.writerow([blog_date, blog_title, blog_link])
